aad981300c61e68cfc9d1ce20fcacadf6f48411b
Link34500 <Link34500>
2026-02-15 18:36:32 -0300

    remove() : remove discord provider

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,8 +2,3 @@ import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({});
 
 export const { signIn, signUp, signOut, useSession } = authClient;
-const discordSignIn = async () => {
-  const data = await authClient.signIn.social({
-    provider: "discord",
-  });
-};
